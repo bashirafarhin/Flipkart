2f6d10dbf1cae56d2e692c34c07cd99f1923364f
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-10-04 19:00:25 +0530

    changes to vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Default is 'dist', you can keep this or specify a different folder
-    emptyOutDir: true, // Clears out old build files before building new ones
-  },
-  root: 'client', // Ensure the root is set to the 'client' directory if your Vite config is inside it
 })
-
