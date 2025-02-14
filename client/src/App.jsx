a71153cae476e42b633780aa9c2117e2b773057b
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-02-14 22:06:33 +0530

    info about new repo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,9 @@
-import Home from './components/Home/Home'
-import DataProvider from './context/DataProvider'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
-import DetailView from './components/Details/DetailView'
-
-//pages
-import RootLayout from './pages/RootLayout'
-import Cart from './components/Cart/Cart'
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<RootLayout/>}>
-        <Route index element={<Home/>}/>
-        <Route path="/product/:id" element={<DetailView/>}/>
-        <Route path="/cart" element={<Cart/>}/>
-    </Route>
-  )
-)
-
 function App() {
   return (
-    <DataProvider>
-      <RouterProvider router={router} />
-    </DataProvider>
+    <div>
+    <h1>This repository has been moved.</h1>
+    <h1>Click [here](https://github.com/bashirafarhin/FlipkartClone) to see the project.</h1>
+    </div>
   )
 }
 
